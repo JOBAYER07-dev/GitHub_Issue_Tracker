8fe094d254455ed63c91511d23c1d62ae9c06223
JOBAYER07-dev <jobayerhosen045@gmail.com>
2026-03-06 22:37:51 +0600

    complete log in page functionality

---
+++ script/login.js
@@ -0,0 +1,18 @@
+const loginBtn = document.getElementById('login-btn');
+
+const loginSection = document.getElementById('login-section');
+
+const mainSection = document.getElementById('main-section');
+
+loginBtn.addEventListener('click', () => {
+  const usernameInput = document.getElementById('input-username').value;
+  const passwordInput = document.getElementById('input-password').value;
+
+  if (usernameInput === "admin" && passwordInput === "admin123") {
+    alert("Login successful!");
+    loginSection.classList.add('hidden');
+    mainSection.classList.remove('hidden');
+  } else {
+    alert("Invalid username or password. Please try again.");
+  }
+})
