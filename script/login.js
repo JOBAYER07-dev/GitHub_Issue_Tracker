c34a131274fb590a71a9cc38e233de0b19818dea
JOBAYER07-dev <jobayerhosen045@gmail.com>
2026-03-07 12:30:38 +0600

    data fatch successfull and show all the cards dynamically

--- script/login.js
+++ script/login.js
@@ -8,11 +8,11 @@ loginBtn.addEventListener('click', () => {
   const usernameInput = document.getElementById('input-username').value;
   const passwordInput = document.getElementById('input-password').value;
 
-  if (usernameInput === "admin" && passwordInput === "admin123") {
-    alert("Login successful!");
+  if (usernameInput === 'admin' && passwordInput === 'admin123') {
+    alert('Login successful!');
     loginSection.classList.add('hidden');
     mainSection.classList.remove('hidden');
   } else {
-    alert("Invalid username or password. Please try again.");
+    alert('Invalid username or password. Please try again.');
   }
-})
+});
