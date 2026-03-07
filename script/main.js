dd5f9e7e59d3d8b287abb91e7983b30267b4d8dc
JOBAYER07-dev <jobayerhosen045@gmail.com>
2026-03-07 16:49:54 +0600

    added modal

--- script/main.js
+++ script/main.js
@@ -8,9 +8,16 @@ const loadData = () => {
     });
 };
 
-const getPriorityClass = status => {
-  if (status === 'open') return 'bg-green-100 text-green-600 rounded-2xl px-4';
-  if (status === 'closed') return 'bg-yellow-100 text-yellow-500 px-4';
+const getStatusClass = status => {
+  if (status === 'open') return 'bg-green-100 text-green-600 rounded-2xl py-1 px-4 text-center';
+  if (status === 'closed') return 'bg-yellow-100 text-yellow-500 py-1 px-4 text-center';
+};
+
+const getPriorityClass = priority => {
+  if (priority === 'high')
+    return 'bg-green-100 text-green-600 rounded-2xl px-4 text-center';
+  if (priority === 'low') return 'bg-red-100 text-red-500 px-4 text-center';
+  if (priority === 'medium') return 'bg-yellow-100 text-yellow-500 px-4 text-center';
 };
 
 const getLabels = labels => {
@@ -22,7 +29,7 @@ const getLabels = labels => {
         return `<p class="bg-green-100 border line-clamp-1 border-green-300 text-green-600 rounded-2xl px-2 py-1"><i class="fa-solid fa-hand-holding-heart"></i> ${label}</p>`;
       if (label === 'enhancement')
         return `<p class="bg-blue-100 border line-clamp-1 border-blue-300 text-blue-600 rounded-2xl px-2 py-1"><i class="fa-regular fa-star"></i> ${label}</p>`;
-      if(label==='documentation') 
+      if (label === 'documentation')
         return `<p class="bg-gray-100 border line-clamp-1 border-gray-300 text-gray-500 rounded-2xl px-2 py-1"><i class="fa-regular fa-file-lines"></i> ${label}</p>`;
       if (label === 'good first issue')
         return `<p class="bg-pink-100 border line-clamp-1 border-pink-300 text-pink-600 rounded-2xl px-2 py-1"><i class="fa-regular fa-handshake"></i> ${label}</p>`;
@@ -30,19 +37,61 @@ const getLabels = labels => {
     .join(' ');
 };
 
+// modal
+const showModal = async id => {
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`,
+  );
+  const data = await res.json();
+  const card = data.data;
+  document.getElementById('details-container').innerHTML = `
+    <div class="space-y-4">
+    <h2 class="text-2xl font-bold">${card.title}</h2>
+    <div class="flex gap-5 text-center">
+      <div class="${getStatusClass(card.status)} p-2 rounded-2xl">
+              <h2 class="font-bold">${card.status}</h2>
+            </div>
+      <div class="flex flex-row gap-2">
+      <p class="text-gray-500 ">${card.assignee}</p>
+      <p class="text-gray-500 ">${card.updatedAt}</p></div>
+    </div>
+
+    <div class="flex gap-3">
+    ${getLabels(card.labels)}
+    </div>
+    <h2 class="text-gray-800 font-medium">${card.description}</h2>
+    <div class="flex gap-5">
+      <div class="flex flex-col gap-3">
+        <p class="text-gray-500">ASSIGNED:</p>
+        <h2 class="font-bold ">${card.assignee}</h2>
+      </div>
+      <div class="flex flex-col gap-3">
+        <p class="text-gray-500">PRIORITYS:</p>
+        <div class="${getPriorityClass(card.priority)} p-2 rounded-2xl">
+              <h2 class="font-bold">${card.priority}</h2>
+            </div>
+      </div>
+    </div>
+  </div>
+  `;
+  my_modal_5.showModal();
+};
+
 const displayCards = cards => {
   // console.log(cards)
   const cardContainer = document.getElementById('main-cards');
   cardContainer.innerHTML = '';
 
+  document.getElementById('issue-count').innerText = cards.length;
+
   cards.forEach(card => {
     const createDiv = document.createElement('div');
     createDiv.innerHTML = `
-     <div class="space-y-10">
+     <div class="space-y-10 cursor-pointer" onclick="showModal(${card.id})">
 
           <div class="flex justify-between">
             <img class="w-10" src="./assets/Open-Status.png" alt="">
-            <div class="${getPriorityClass(card.status)} p-2 rounded-2xl">
+            <div class="${getStatusClass(card.status)} p-2 rounded-2xl">
               <h2 class="font-bold">${card.status}</h2>
             </div>
           </div>
@@ -84,23 +133,22 @@ const activeBtn = activeId => {
 
   document.getElementById(activeId).classList.add('btn-primary');
   document.getElementById(activeId).classList.remove('btn-outline');
-}
+};
 
-const allBtn = () => { 
+const allBtn = () => {
   displayCards(allData);
-activeBtn('all-btn')
-}
-  const openBtn = () => {
-    const openCards = allData.filter(card => card.status === 'open');
-    displayCards(openCards);
-    activeBtn('open-btn')
-  };
+  activeBtn('all-btn');
+};
+const openBtn = () => {
+  const openCards = allData.filter(card => card.status === 'open');
+  displayCards(openCards);
+  activeBtn('open-btn');
+};
 
-  const closedBtn = ()=>{
-    const closedCards = allData.filter(card => card.status === 'closed');
-    displayCards(closedCards);
-    activeBtn('closed-btn')
-  }
-
+const closedBtn = () => {
+  const closedCards = allData.filter(card => card.status === 'closed');
+  displayCards(closedCards);
+  activeBtn('closed-btn');
+};
 
 loadData();
