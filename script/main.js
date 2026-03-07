f621686c469a4c32b08a4a42c1d773ac4eb75d22
JOBAYER07-dev <jobayerhosen045@gmail.com>
2026-03-07 15:08:10 +0600

    all open and closed button funtionality added and dynamically show data on button click

--- script/main.js
+++ script/main.js
@@ -1,29 +1,31 @@
+let allData = [];
 const loadData = () => {
   fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
     .then(res => res.json())
-    .then(data => displayCards(data.data));
+    .then(data => {
+      allData = data.data;
+      displayCards(allData);
+    });
 };
 
-const getPriorityClass = priority => {
-  if (priority === 'high') return 'bg-red-100 text-red-600 rounded-2xl px-4';
-  if (priority === 'medium') return 'bg-yellow-100 text-yellow-500 px-4';
-  if (priority === 'low') return 'bg-gray-200 text-gray-500 px-4';
-  return 'bg-gray-100 text-gray-500';
+const getPriorityClass = status => {
+  if (status === 'open') return 'bg-green-100 text-green-600 rounded-2xl px-4';
+  if (status === 'closed') return 'bg-yellow-100 text-yellow-500 px-4';
 };
 
 const getLabels = labels => {
   return labels
     .map(label => {
       if (label === 'bug')
-        return `<p class="bg-red-100 border border-red-300 text-red-600 rounded-2xl px-2 py-1"><i class="fa-solid fa-bug"></i> ${label}</p>`;
+        return `<p class="bg-red-100 border line-clamp-1 border-red-300 text-red-600 rounded-2xl px-2 py-1"><i class="fa-solid fa-bug"></i> ${label}</p>`;
       if (label === 'help wanted')
-        return `<p class="bg-green-100 border border-green-300 text-green-600 rounded-2xl px-2 py-1"><i class="fa-solid fa-hand-holding-heart"></i> ${label}</p>`;
+        return `<p class="bg-green-100 border line-clamp-1 border-green-300 text-green-600 rounded-2xl px-2 py-1"><i class="fa-solid fa-hand-holding-heart"></i> ${label}</p>`;
       if (label === 'enhancement')
-        return `<p class="bg-blue-100 border border-blue-300 text-blue-600 rounded-2xl px-2 py-1"><i class="fa-regular fa-star"></i> ${label}</p>`;
+        return `<p class="bg-blue-100 border line-clamp-1 border-blue-300 text-blue-600 rounded-2xl px-2 py-1"><i class="fa-regular fa-star"></i> ${label}</p>`;
       if(label==='documentation') 
-        return `<p class="bg-gray-100 border border-gray-300 text-gray-500 rounded-2xl px-2 py-1"><i class="fa-regular fa-file-lines"></i> ${label}</p>`
+        return `<p class="bg-gray-100 border line-clamp-1 border-gray-300 text-gray-500 rounded-2xl px-2 py-1"><i class="fa-regular fa-file-lines"></i> ${label}</p>`;
       if (label === 'good first issue')
-        return `<p class="bg-pink-100 border border-pink-300 text-pink-600 rounded-2xl px-2 py-1"><i class="fa-regular fa-handshake"></i> ${label}</p>`;
+        return `<p class="bg-pink-100 border line-clamp-1 border-pink-300 text-pink-600 rounded-2xl px-2 py-1"><i class="fa-regular fa-handshake"></i> ${label}</p>`;
     })
     .join(' ');
 };
@@ -40,8 +42,8 @@ const displayCards = cards => {
 
           <div class="flex justify-between">
             <img class="w-10" src="./assets/Open-Status.png" alt="">
-            <div class="${getPriorityClass(card.priority)} p-2 rounded-2xl">
-              <h2 class="font-bold">${card.priority}</h2>
+            <div class="${getPriorityClass(card.status)} p-2 rounded-2xl">
+              <h2 class="font-bold">${card.status}</h2>
             </div>
           </div>
 
@@ -66,5 +68,39 @@ const displayCards = cards => {
   });
 };
 
+const activeBtn = activeId => {
+  const allBtn = document.getElementById('all-btn');
+  const openBtn = document.getElementById('open-btn');
+  const closedBtn = document.getElementById('closed-btn');
+
+  allBtn.classList.add('btn-outline');
+  allBtn.classList.remove('btn-primary');
+
+  openBtn.classList.add('btn-outline');
+  openBtn.classList.remove('btn-primary');
+
+  closedBtn.classList.add('btn-outline');
+  closedBtn.classList.remove('btn-primary');
+
+  document.getElementById(activeId).classList.add('btn-primary');
+  document.getElementById(activeId).classList.remove('btn-outline');
+}
+
+const allBtn = () => { 
+  displayCards(allData);
+activeBtn('all-btn')
+}
+  const openBtn = () => {
+    const openCards = allData.filter(card => card.status === 'open');
+    displayCards(openCards);
+    activeBtn('open-btn')
+  };
+
+  const closedBtn = ()=>{
+    const closedCards = allData.filter(card => card.status === 'closed');
+    displayCards(closedCards);
+    activeBtn('closed-btn')
+  }
+
+
 loadData();
-displayCards();
